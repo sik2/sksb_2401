d6f79f84fa7eafa8bd230c5d324376b7049a0251
sik2 <bickwang123@gmail.com>
2024-01-21 03:36:59 +0900

    스프링 JPA Security 기본 설정

--- frontapp/src/routes/article/+page.svelte
+++ frontapp/src/routes/article/+page.svelte
@@ -1,1 +1,7 @@
+<script>
+	let res = fetch('http://localhost:8090/api/v1/articles');
+
+	console.log(res);
+</script>
+
 <div>게시글 목록 페이지</div>
---
+++ src/main/java/com/example/sksb/domain/article/entity/Article.java
@@ -0,0 +1,11 @@
+package com.example.sksb.domain.article.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+
+@Getter
+@AllArgsConstructor
+public class Article {
+    private Long id;
+}

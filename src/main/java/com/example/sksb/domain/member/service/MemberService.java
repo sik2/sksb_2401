043112354b51f41ff6771fcbd6b446cef0992862
sik2 <bickwang123@gmail.com>
2024-02-01 11:46:50 +0900

    member domain

---
+++ src/main/java/com/example/sksb/domain/member/service/MemberService.java
@@ -0,0 +1,36 @@
+package com.example.sksb.domain.member.service;
+
+import com.example.sksb.domain.member.entity.Member;
+import com.example.sksb.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public Optional<Member> findByUsername(String username) {
+        return memberRepository.findByUsername(username);
+    }
+
+    @Transactional
+    public void join(String username, String password) {
+        Member member = Member.builder()
+                .username(username)
+                .password(passwordEncoder.encode(password))
+                .build();
+
+        memberRepository.save(member);
+    }
+
+    public boolean passwordMatches(Member member, String password) {
+        return passwordEncoder.matches(password, member.getPassword());
+    }
+}
